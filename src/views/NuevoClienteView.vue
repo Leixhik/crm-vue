d926be98bd9e4f5f2f55de38ca24ae14bd7696c0
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-12-08 09:16:10 -0600

    Se refactorizó el componente RouterLink para mejorar la estructura y se actualizó su uso en las vistas de Clientes y Nuevo Cliente.

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,14 +1,11 @@
 <script setup>
-import { RouterLink } from "vue-router";
+  import RouterLink from '@/components/UI/RouterLink.vue';
 </script>
 
 <template>
   <div>
-    <div>
-      <RouterLink
-       :to="{ name: 'inicio' }"
-       class="block md:inline-block rounded bg-blue-600 py-2 px-3"
-       > Volver </RouterLink>
+    <div class="flex justify-end">
+      <RouterLink to="inicio"> Volver </RouterLink>
     </div>
     <h1 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h1>
   </div>
